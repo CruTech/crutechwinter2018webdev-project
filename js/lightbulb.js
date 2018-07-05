c1aa8d98f8ee73d6f66f79ac640b535fc7eaed7a
shadowolf <shadowolfbusiness@gmail.com>
2018-07-05 17:49:56 +1000

    Adding.

---
+++ js/lightbulb.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+function lightsupBOIS() {
+    document.getElementById('lightbulbgif').src='img/pic_bulbon.gif';
+	alert('The lights are on, NOT THE BLINDING LIIGHT PLEASE?!?!?!?');
+}
+
+function lightsdown() {
+    document.getElementById('lightbulbgif').src='img/pic_bulbon.gif';
+	alert('Ahh, that\'s better... Night night big bois.');
+}
